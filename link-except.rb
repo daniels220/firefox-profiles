d1321dba6ea6cfb26187871e0ac87bc7dd56dc24
Daniel <daniels220@gmail.com>
2012-02-09 22:48:49 -0500

    Initial commit

---
+++ link-except.rb
@@ -0,0 +1,42 @@
+#!/opt/local/bin/ruby
+
+require 'pathname'
+
+class Pathname
+	alias_method :rpf, :relative_path_from
+	def components
+		comps = []
+		each_filename { |f| comps.push(f) }
+		comps
+	end
+end
+
+if (ARGV[0] == '-h')
+	ARGV.shift
+	def link(from,to); to.make_link(from); end
+else
+	def link(from,to); to.make_symlink(from); end
+end
+
+$from = Pathname.new(ARGV.shift)
+$to = Pathname.new(ARGV.shift)
+$to.mkdir
+$to = $to.realpath
+$except = ARGV.map { |f| ex = Pathname.new(f); $to+(ex.relative? ? ex : ex.rpf($from)) }
+$from = $from.realpath
+
+def dir_recurse(dir)
+	dir.each_child do |from_f|
+		to_f = $to+from_f.rpf($from)
+		next if $except.include?(to_f)
+		if from_f.file? || (!$except.empty? && $except.all? { |f| f.rpf(to_f).components[0] == '..' })
+			link(from_f,to_f)
+		elsif from_f.directory?
+			to_f.mkdir
+			dir_recurse(from_f)
+		else
+		end
+	end
+end
+
+dir_recurse($from)
---
+++ make-ff-profile.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [[ $# == 1 ]]; then
+
+# Put icons in the same folder as the script
+# This is just to make getting at them easier
+cd $(dirname $0)
+# Change this line to change output directory
+PREFIX=/Applications
+FF_copy=$PREFIX/Firefox-$1.app
+# Change this to the location of Firefox.app
+FF=/Applications/Firefox.app
+
+# Files we're going to exclude
+info_strings=Contents/Resources/en.lproj/InfoPlist.strings
+icon=Contents/Resources/firefox.icns
+exec_file=Contents/MacOS/firefox
+
+# Remove any previous copies
+rm -r $FF_copy
+
+# Create a symlinked copy of Firefox, excluding the files we're going to change
+# ($exec_file-dummy means that MacOS will get deep-linked but the executable itself will also be linked)
+./link-except.rb $FF $FF_copy $icon $info_strings $exec_file-dummy
+
+# Make FF show up as 'firefox-{profilename} in `top` or the like
+# Activity Monitor uses the CFBundleName below
+exec_suffix=-$(tr A-Z a-z <<< "$1")
+mv $FF_copy/$exec_file{,$exec_suffix}
+
+# Copy in the icon
+cp Firefox-$1.icns $FF_copy/$icon
+
+# Edit InfoPlist.strings (change menu-bar name)
+# Strings files are meant for international text and are UTF-16, which sed doesn't understand
+iconv -f utf-16 -t utf-8 <$FF/$info_strings | sed -Ee "/CFBundleName/s/Firefox/Firefox-$1/" | iconv -f utf-8 -t utf-16 >$FF_copy/$info_strings
+
+# Wrap executable
+echo -n '#!/bin/sh
+exec `dirname $0`/firefox'$exec_suffix' -p '$1' -no-remote' > $FF_copy/$exec_file
+chmod +x $FF_copy/$exec_file
+
+else for pname in $@; do $0 $pname; done; fi
